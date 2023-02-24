644306421cb7e979830c785f08bad7720cc5efd9
Kevin Ngkaion <kevinngkaion@gmail.com>
2023-02-24 13:03:40 -0800

    Added department and category tables and set up django/admin

--- okeefeTM/okeefeTM/settings.py
+++ okeefeTM/okeefeTM/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'taskManager'
-
+    'taskManager',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                
             ],
         },
     },
--- okeefeTM/taskManager/models.py
+++ okeefeTM/taskManager/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Department(models.Model):
+  dep_name = models.CharField(max_length=30)
+
+class Category(models.Model):
+  cat_name = models.CharField(max_length=30)
