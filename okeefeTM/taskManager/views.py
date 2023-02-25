715594e9e6c7a008e12e735fda09039abd02a8d7
Tuscun McFarlen <tuscun54@gmail.com>
2023-02-25 14:11:07 -0800

    Added Task Dashboard Functionality

--- okeefeTM/taskManager/views.py
+++ okeefeTM/taskManager/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+
+from .forms import TaskForm
+from .models import *
 from django.http import HttpResponse
+
 
 # Create your views here.
 def index(request):
-  return render(request, 'navbar.html')
+    task = Task.objects.all()
+    taskform = TaskForm
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': taskform,
+               'task': task}
+    return render(request, 'home.html', context)
+
