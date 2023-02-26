9eb69263656eebe2e1e12807f0ab90dcfddeccd9
Tzu-Ai Shen <shaz2368@gmail.com>
2023-02-26 17:07:48 +0800

    login html and urls change

--- okeefeTM/taskManager/urls.py
+++ okeefeTM/taskManager/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index')
+  path('', views.index, name='index'),
+  path('accounts/login/', views.login, name='login'),
+  path('',include('django.contrib.auth.urls'))
+  #path('account/profile/', home)
 ]
--- okeefeTM/taskManager/views.py
+++ okeefeTM/taskManager/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
   return render(request, 'navbar.html')
+
+def home(request):
+  return render(request,'home.html')
+
+def login_user(request):
+  return render(request, 'authenticate/login.html', {})
