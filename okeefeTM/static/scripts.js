96289eb895a4ee9874963bb53a3c1fcc288b441c
Kevin Ngkaion <kevinngkaion@gmail.com>
2023-02-27 23:41:29 -0800

    Updated the task table to have dynamic color changes depending on the task status and priority

--- okeefeTM/static/scripts.js
+++ okeefeTM/static/scripts.js
@@ -1,7 +1,36 @@
 $(document).ready(function () {
-    $('#tasksTable').DataTable();
-
-
+    let tStats = Object.values($('.task-status'))
+    let tPrios = Object.values($('.task-prio'))
+    tStats.forEach((tStat) => {
+        switch(tStat.textContent){
+            case "Unassigned":
+                tStat.classList.add('bg-secondary');
+                break;
+            case "Complete":
+                tStat.classList.add('bg-success');
+                break;
+            case "In Progress":
+                tStat.classList.add('bg-warning');
+                break;
+            case "Not Started":
+                tStat.classList.add('bg-danger');
+                break;
+        }
+    })
+    tPrios.forEach((tPrio) => {
+        switch(tPrio.textContent){
+            case "Low":
+                tPrio.classList.add('text-success');
+                break;
+            case "Medium":
+                tPrio.classList.add('text-warning');
+                break;
+            case "High":
+                tPrio.classList.add('text-danger');
+                break;
+        }
+    })
+    $('#tasksTable').DataTable(); //This needs to be at the end so that the formatting can be done first before the table is output
 });
 
 function showTaskInfo(name, description){
