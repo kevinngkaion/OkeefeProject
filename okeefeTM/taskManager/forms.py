af854b5cbb1fb742a5cc05f1a53a05314cea5de6
Tuscun McFarlen <tuscun54@gmail.com>
2023-03-09 11:10:51 -0800

    forms

--- okeefeTM/taskManager/forms.py
+++ okeefeTM/taskManager/forms.py
@@ -5,11 +5,9 @@ from .models import Task
 from django import forms
 from django.contrib.auth.models import User
 
-
-class TaskForm(ModelForm):    
+class TaskForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         self.fields['name'].widget.attrs['class'] = 'form-control'
         self.fields['user'].widget.attrs['class'] = 'form-select'
         self.fields['category'].widget.attrs['class'] = 'form-select'
@@ -41,7 +39,40 @@ class TaskForm(ModelForm):
             'desc',
             ]
 
+class EditTaskForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['user'].widget.attrs['class'] = 'form-select'
+        self.fields['category'].widget.attrs['class'] = 'form-select'
+        self.fields['priority'].widget.attrs['class'] = 'form-select'
+        self.fields['date_due'].widget = widgets.DateInput(
+            attrs={'type': 'date', 'class': 'form-control'}
+        )
+        self.fields['repeat'].widget.attrs['class'] = 'form-select'
+        self.fields['desc'].widget = widgets.Textarea(
+            attrs={'class': 'form-control', 'rows': '5'}
+        )
 
+    class Meta:
+        model = Task
+        labels = {
+            'status': 'Status',
+            'user': 'Assign To',
+            'desc': 'Description',
+            'date_due': 'Date Due',
+            'repeat': 'Is this a recurring task?',
+            'notes': 'note'
+            }
+        fields = [
+            'status',
+            'user',
+            'category',
+            'priority',
+            'date_due',
+            'repeat',
+            'desc',
+            'note',
+            ]
 class CreateUserForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         fields = UserCreationForm.Meta.fields + ("email", "first_name", "last_name")
