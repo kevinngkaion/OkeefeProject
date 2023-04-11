cfbbe47c8bbebcaa22ba24bb1ec752f26bced693
Tzu-Ai Shen <shaz2368@gmail.com>
2023-04-11 13:06:45 +0800

    forget password bugs fix

--- okeefeTM/taskManager/views.py
+++ okeefeTM/taskManager/views.py
@@ -1,17 +1,13 @@
-from django.contrib.auth.decorators import login_required
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
+
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model, authenticate, login, logout
-from django.urls import reverse
-from django.utils.encoding import force_bytes, force_str
-from django.utils.html import strip_tags
+from django.urls import reverse, reverse_lazy
 from .forms import *
 from .models import *
 from django.http import JsonResponse
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.contrib.auth.models import User
-from django.contrib import messages, auth
+from django.contrib import messages
 from django.utils import timezone
 from datetime import date, timedelta
 from dateutil.relativedelta import relativedelta
@@ -383,9 +379,9 @@ def change_password(request, uidb64, token):
             else:
                 user.set_password(password1)
                 user.save()
-                login(request, user)
                 return render(request, 'change_password.html', {'success': 'Password reset successful.'})
+            return redirect(reverse_lazy('login'))
         return render(request, 'change_password.html', {'user': user})
     else:
         messages.error(request, 'Invalid password reset link')
-        return redirect('login')
+        return redirect(reverse_lazy('login'))
